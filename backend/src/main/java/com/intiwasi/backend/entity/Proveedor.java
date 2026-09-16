fcf115de7dd5d3ab6e5760e0419cb6cf64d5259f
Kevin-Andr3 <kevin.delacruz.vi@gmail.com>
2026-09-16 18:25:31 -0500

    Crear entidad Proveedor

---
+++ backend/src/main/java/com/intiwasi/backend/entity/Proveedor.java
@@ -0,0 +1,37 @@
+package com.intiwasi.backend.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Proveedores")
+public class Proveedor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "IdProveedor")
+    private Integer idProveedor;
+
+    @Column(name = "NomProveedor", length = 100, nullable = false)
+    private String nomProveedor;
+
+    @Column(name = "RUC", length = 11, nullable = false, unique = true)
+    private String ruc;
+
+    @Column(name = "Contacto", length = 100)
+    private String contacto;
+
+    @Column(name = "Telefono", length = 20)
+    private String telefono;
+
+    @Column(name = "Direccion", length = 150)
+    private String direccion;
+
+    @Column(name = "Estado", nullable = false)
+    private Integer estado = 1;
+}
