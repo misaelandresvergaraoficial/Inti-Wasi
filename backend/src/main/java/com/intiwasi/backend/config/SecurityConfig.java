4241a074ba7e67d5e3c86c9e950940a9f0ad54f0
Hetsvik <rodrigo.ochoa333@gmail.com>
2026-09-17 22:26:52 -0500

    Se cambio el numero de puerto

--- backend/src/main/java/com/intiwasi/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/intiwasi/backend/config/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));   // origen de Angular
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));   // origen de Angular
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
